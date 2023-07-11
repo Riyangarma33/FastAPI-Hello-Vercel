4a4c71586f2070126e45dc05aab2e9128f0910af
Muhammad Garma <mgarma76@gmail.com>
2023-07-11 17:04:41 +0700

    Add /hello route

--- server/api.py
+++ server/api.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from server.routes import router as HelloRouter
 
 app = FastAPI()
 
@@ -7,3 +8,6 @@ app = FastAPI()
 @app.get("/", tags=["Root"])
 async def read_root():
     return JSONResponse({"message": "Welcome to Hello World"}, status_code=200)
+
+
+app.include_router(HelloRouter)
